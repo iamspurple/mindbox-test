be897030a7c02480418af353756f8077ec01870f
Sabina Gasanova <sabina21gasanova@yandex.ru>
2025-07-07 18:43:31 +0300

    small fix

--- src/services/useTodos.tsx
+++ src/services/useTodos.tsx
@@ -1,29 +1,28 @@
 import { useEffect, useMemo, useState } from "react";
 import type { ITodoItem, TFilter } from "../types";
+import { v4 as uuidv4 } from "uuid";
 
 export const useTodos = () => {
   const [todoList, setTodoList] = useState<ITodoItem[]>([
-    { id: 1, title: "Выполнить тестовое", completed: true },
-    { id: 2, title: "Откликнуться на вакансию", completed: true },
-    { id: 3, title: "Позвать кандидата на собеседование", completed: false },
+    { id: "1", title: "Выполнить тестовое", completed: true },
+    { id: "2", title: "Откликнуться на вакансию", completed: true },
+    { id: "3", title: "Позвать кандидата на собеседование", completed: false },
   ]);
   const [filter, setFilter] = useState<TFilter>("all");
-  const [itemsLeft, setItemsLeft] = useState<number>();
+  const [itemsLeft, setItemsLeft] = useState<number>(0);
 
   useEffect(() => {
     setItemsLeft(todoList.filter((item) => !item.completed).length);
   }, [todoList]);
 
   const addTodo = (title: string) => {
-    if (title) {
-      setTodoList((prev) => [
-        ...prev,
-        { id: Date.now(), title, completed: false },
-      ]);
+    if (title && title.trim()) {
+      const uuid = uuidv4();
+      setTodoList((prev) => [...prev, { id: uuid, title, completed: false }]);
     }
   };
 
-  const toggleCompleted = (id: number) => {
+  const toggleCompleted = (id: string) => {
     setTodoList((prev) =>
       prev.map((item) => {
         if (item.id === id) {
